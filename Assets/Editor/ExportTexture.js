297275f3e963f36ac37d22627f4e1a216d88d03f
Chad Macon <chad@jellogroup.com>
2015-08-18 00:26:28 -0600

    ExportTexture.js fix angle
    Note the uncomment at top that gives a even more accurate path. I was trying to use Export Texture with the Apply Splatmap in this same editor folder and found the terrain exported texture was flipped wrong. This adds a crude work around to export terrains that may give you this problem.

--- Assets/Editor/ExportTexture.js
+++ Assets/Editor/ExportTexture.js
@@ -1,17 +1,31 @@
-// For save splatmap as PNG file.
+/*
+As well as GetPixel, Texture2D also has a GetPixelBilinear method. Image processing transforms like this inevitably want non-integer pixel coordinates from the source image. GetPixelBilinear handles this for you by returning a weighted average of four neighbouring pixels. The rotated texture will look much better if you use this rather than rounding pixel coordinates off to the nearest integer and using GetPixel.
+*/
+
 import System.IO;
 
 @MenuItem("Assets/Export Texture")
 
 static function Apply () {
+
+   var rotImage:int = 270;
+   var ang = rotImage * Mathf.Deg2Rad;
    
    var texture : Texture2D = Selection.activeObject as Texture2D;
-   if (texture == null)
-   {
-      EditorUtility.DisplayDialog("Select Texture", "You Must Select a Texture first!", "Ok");
-      return;
-   }
+   var ntexture = new Texture2D(texture.width,texture.height);
    
-   var bytes = texture.EncodeToPNG();
-   File.WriteAllBytes(Application.dataPath + "/exported_texture.png", bytes);
+   for (var i = 0; i < texture.height; i++) { 
+      for (var j = 0; j < texture.width; j++) {
+           
+         var x_new : int=(j) * Mathf.Cos(ang) - (i)*Mathf.Sin(ang);
+         var y_new : int=(i) * Mathf.Cos(ang) + (j)*Mathf.Sin(ang);
+           
+         ntexture.SetPixel ( x_new, y_new, texture.GetPixel(i,j));
+      }
+   }
+   ntexture.Apply();
+   
+   
+   var bytes = ntexture.EncodeToPNG();
+   File.WriteAllBytes(Application.dataPath + "/exported_texture_270.png", bytes);
 }
